13cb914e0d656ae080031861b3db7b834cf85119
Tobias080902 <25546@ma-web.nl>
2018-11-07 13:44:53 +0100

    Bug fix and transition

--- src/com/miku/Vending/Main.java
+++ src/com/miku/Vending/Main.java
@@ -6,25 +6,29 @@ public class Main {
     static List<Item> items = new ArrayList<>();
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         boolean VendingMachine = true;
 
         Scanner scan = new Scanner(System.in);
         String input = "";
+        //Arrays
+        String[] commands = {"yes", "no", "help"};
+        String[] orders = {"help", "cola", "fanta", "water", "7up"};
 
-        String[] commands = {"yes", "no", "help", "exit"};
+        //String
+        String Drinks = "What would you like to drink?";
+        String MainQuestion = "Would you like something to drink? Type: ";
 
-        String[] orders = {"cola", "fanta", "water", "7up"};
 
         items.add(new Item("cola", 1.50));
         items.add(new Item("fanta", 1.50));
 
 
-        while ( VendingMachine ) //VendingMachine == true
+        while (VendingMachine) //VendingMachine == true
         {
 
-            System.out.println("Would you like something to drink? Type: ");
+            System.out.println(MainQuestion);
 
             for (int i = 0; i < commands.length; i++) {
                 System.out.println(" - " + commands[i]);
@@ -32,118 +36,66 @@ public class Main {
 
             //INPUT CONTROL:
             boolean geldigeInput = false;
-            while (geldigeInput == false)
-            {
+            while (geldigeInput == false) {
                 input = scan.nextLine();
                 input = input.toLowerCase();
 
-                for(int i = 0; i < orders.length || i < commands.length; i++)                {
-                    if(input.equals(orders[i]) || input.equals(commands[i]))
-                    {
-                        geldigeInput = true;
-                        break; // Breek de huidige loop
-                    }
+                if (input.equals(commands[0])) {
+                    System.out.println(Drinks);
+                    geldigeInput = true;
+
+                } else if (input.equals(commands[1])) {
+                    geldigeInput = true;
+                    VendingMachine = false;
+                    System.out.println("chose no");
+
+                } else if (input.equals(commands[2])) {
+                    System.out.println(Arrays.toString(commands));
+                    geldigeInput = false;
+
+
                 }
+                continue;
+
             }
 
 
+            boolean order = false;
+            while (order == false) {
+                input = scan.nextLine();
+                input = input.toLowerCase();
+
+                if (input.equals(orders[0])) {
+                    order = false;
+                    System.out.println(Arrays.toString(commands));
 
 
-            boolean Order = false;
-            if( input.equals( orders[0])) //cola
-            {
-                if(isItem(input))
-                {
+                } else if (input.equals(orders[1])) {
+                    order = true;
+                    System.out.println("Here is your " + input);
+                    System.out.println("Thanks for ordering " + "NULL");
 
-                    if (getItem(input) == null) {
-                        throw new NullPointerException();
-                    }
 
-                    //Order Complete
-                    Order = true;
+                } else if (input.equals(orders[2])) {
+                    order = true;
+                    System.out.println("Here is your " + input);
+                    System.out.println("Thanks for ordering " + "NULL");
+
+
+                } else if (input.equals(orders[3])) {
+                    order = true;
+                    System.out.println("Here is your " + input);
+                    System.out.println("Thanks for ordering " + "NULL");
+
+
+                } else if (input.equals(orders[4])) {
+                    order = true;
+                    System.out.println("Here is your " + input);
+                    System.out.println("Thanks for ordering " + "NULL");
+
+
                 }
-            }
-            else if( input.equals( orders[1])) //fanta
-            {
-                if(isItem(input)) {
-                    //Order Complete
-
-                    if (getItem(input) == null) {
-                        throw new NullPointerException();
-                    }
-
-                    Order = true;
-                }
-
-            }
-            else if( input.equals( orders[2])) //fanta
-            {
-                if(isItem(input)) {
-                    //Order Complete
-
-                    if (getItem(input) == null) {
-                        throw new NullPointerException();
-                    }
-
-                    Order = true;
-                }
-
-            }
-            else if( input.equals( orders[3])) //fanta
-            {
-                if(isItem(input)) {
-                    //Order Complete
-
-                    if (getItem(input) == null) {
-                        throw new NullPointerException();
-                    }
-
-                    Order = true;
-                }
-
-            }
-            else
-            {
-                //exit
-                VendingMachine = false;
-                continue; //Stopt de verdere code in de loop en gaat verder met de volgende iteratie
-
-            }
-
-            // Eind resultaat tonen
-
-            System.out.println("Here is your " + input);
-            System.out.println("Thanks for ordering " + "NULL");
-
-            if(Order) {
-                System.out.println("Thanks!");
-            }
-            else {
-                System.out.println("Wrong input try again! Use: " + orders + "as input");
-            }
-
-
-        }
-
-    }
-
-    public static boolean isItem(String name) {
-        for (Item item : items) {
-            if (item.name.equalsIgnoreCase(name)) {
-                return true;
             }
         }
-
-        return false;
-    }
-
-    public static Item getItem(String name) {
-        for (Item item : items) {
-            if (item.name.equalsIgnoreCase(name)) {
-                return item;
-            }
-        }
-
-        return null;
     }
 }
